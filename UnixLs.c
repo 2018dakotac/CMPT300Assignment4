130f580cd8061c263b6742ad544b495f0c27b345
Sanz1819 <saniyaghorpade78@gmail.com>
2021-08-06 18:10:32 +0530

    Added opendir code

--- UnixLs.c
+++ UnixLs.c
@@ -3,6 +3,30 @@
 #include <stdio.h>
 #include <grp.h>
 #include <pwd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void ls_func(const char *dir, int a, int b){
+    
+    DIR *directory = opendir(dir); //opens the directory. Included in dirent.h
+    if(!directory){
+        if(errno == ENOENT){
+            perror("This Directory does not exist"); //Not found
+        }
+        
+        else{
+            perror("This Directory is Unreadable"); //No read permission
+        }
+        
+        exit(EXIT_FAILURE);
+    }
+    
+    struct dirent *readdir(DIR *dirp);
+    
+}
+
 
 int main () {
     
